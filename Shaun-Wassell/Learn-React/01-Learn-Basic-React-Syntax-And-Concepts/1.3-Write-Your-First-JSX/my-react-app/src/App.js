c25021b55081981357eb5523a42443353eb5f972
Srinivasa Rao Gurram <srinivasaraogurram@users.noreply.github.com>
2024-06-03 00:37:49 +0000

    Added 1.3 Wirte your first JSX component

---
+++ Shaun-Wassell/Learn-React/01-Learn-Basic-React-Syntax-And-Concepts/1.3-Write-Your-First-JSX/my-react-app/src/App.js
@@ -0,0 +1,32 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+
+  let isMorning = (new Date()).getHours() < 12;
+  let greetingElement = isMorning ? <h3> Good Morning! </h3> : <h3> Good Evening!  </h3>;
+  let adjective = 'cool';
+  let url = "https://reactjs.org"; 
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        {greetingElement}
+        <p>
+
+         this is so {adjective}!
+        </p>
+        <a
+          className="App-link"
+          href={url}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ Shaun-Wassell/Learn-React/01-Learn-Basic-React-Syntax-And-Concepts/1.3-Write-Your-First-JSX/readme.md
@@ -0,0 +1,35 @@
+![Write your first JSX](image.png)
+![alt text](image-1.png)
+Importing App Component in Index.js
+![alt text](image-2.png)
+
+Creating todo list component
+
+<TodoList/>
+
+Greeting the user based on the time
+`` 
+let isMorning = (new Date()).getHours()< 12;
+let greetingElement = isMorning ? <h3> Good Morning! </h3> : <h3> Good Evening! 
+```
+
+![alt text](image-3.png)
+
+insert the variable enclosing with curly braces
+{greetingElement}
+
+![alt text](image-4.png)
+Showcasing {greetingElement} along with the code
+
+![alt text](image-5.png)
+
+Creating a variable adjective = 'cool' 
+and inserting below
+![alt text](image-6.png)
+
+![SVG as variable](image-7.png)
+showing url as link
+![alt text](image-8.png)
+
+
+
